9381bf03ffd6d1ea3b84eb8ee5d7b1e91b10e7ff
Felipe Blassioli <felipeblassioli@gmail.com>
2015-05-12 22:50:20 -0300

    cleaning

--- ep2/js/trackball.js
+++ ep2/js/trackball.js
@@ -115,8 +115,7 @@ VirtualTrackBall.prototype = {
 		this.height = height;
 		this.r = this.min(width, height)/2;
 		this.q = new Quaternion();
-		this.start = null;
-		this.end = null;
+
 		this.startW = null;
 		this.endW = null;
 	},
@@ -146,27 +145,6 @@ VirtualTrackBall.prototype = {
 		v.z = z;
 		
 		return v.nor();
-	},
-	
-	//get start position and create track ball vector for start position
-	setRotationStart:function(win_x, win_y){
-		this.start = this.getTrackBallVector(win_x, win_y);
-	},
-	
-	
-	//get new position, create track ball vector, and base on the old vector and new vector,
-	//calculate the rotation axial, and angle
-	rotateTo:function(win_x, win_y){
-		var end = this.getTrackBallVector(win_x, win_y);
-		var axis = end.clone().cross(this.start).nor();
-		var dis = 0-end.clone().sub(this.start).len()*2;
-		//var dis = end.clone().sub(this.start).len()*2;
-
-		var curRP = new Quaternion();
-		curRP.setFromAxisAngle(axis, dis);
-		
-		this.q = curRP.multiply(this.q);
-		this.start=end;
 	},
 	
 	//convert it to Quaternion, and merger to the old one, convert to matrix and return
